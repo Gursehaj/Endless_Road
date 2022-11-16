9fd368e6e103031691af3efd6536b7579e98b4e2
Gursehaj <gursehaj@bigthinx.com>
2022-11-16 20:27:53 +0530

    adding control points

--- Assets/Scripts/BezierCreator.cs
+++ Assets/Scripts/BezierCreator.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 class BezierCreator : MonoBehaviour
 {
-    public GameObject point1;
-    public GameObject point2;
-    public GameObject controlPoint1;
-    public GameObject controlPoint2;
-
-    //Vector3 p1;
-    //Vector3 p2;
-    //Vector3 c1;
-    //Vector3 c2;
+    [SerializeField]
+    bool isDebugging;
 
     List<Vector3> inputPoints = new List<Vector3>();
     List<Vector3> controlPoints = new List<Vector3>();
-
-
-    private List<Vector3> bezPoints;
-
-    private LineRenderer lineRenderer;
+    LineRenderer lineRenderer;
 
     [Range(1, 50)]
     public int stepSize;
@@ -43,19 +32,6 @@ class BezierCreator : MonoBehaviour
             inputPoints.Add(pos);
             DrawBezier();
         }
-
-        //p1 = point1.transform.position;
-        //p2 = point2.transform.position;
-
-        //c1 = controlPoint1.transform.position;
-        //c2 = controlPoint2.transform.position;
-        //lineRenderer.positionCount = stepSize + 1;
-        //for (int i = 0; i <= stepSize; i++)
-        //{
-        //    var t = i / (float)stepSize;
-        //    var point = Mathf.Pow((1 - t), 3) * p1 + 3 * Mathf.Pow((1 - t), 2) * t * c1 + 3 * (1 - t) * Mathf.Pow(t, 2) * c2 + Mathf.Pow(t, 3) * p2;
-        //    lineRenderer.SetPosition(i, point);
-        //}
     }
 
     private void DrawBezier()
@@ -68,23 +44,29 @@ class BezierCreator : MonoBehaviour
                 var p1 = inputPoints[j];
                 var p2 = inputPoints[j + 1];
 
-                var c1 = Vector3.Cross((p2 - p1), Vector3.up).normalized * 10;
-                var c2 = Vector3.Cross((p1 - p2), Vector3.up).normalized * 10;
+                var c1 = Vector3.Cross((p2 - p1), Vector3.up);
+                var c2 = Vector3.Cross((p1 - p2), Vector3.up);
+
+                Debug.DrawLine(p1, c1);
+                Debug.DrawLine(p2, c2);
+
+                //var ang1 = Vector3.SignedAngle(p1 , c1 , Vector3.up);
+                //var ang2 = Vector3.SignedAngle(p2 , c2 , Vector3.up);
+
+                //print(ang1);
+                //print(ang2);
 
                 controlPoints.Add(c1);
                 controlPoints.Add(c2);
-                var sp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                sp.transform.SetPositionAndRotation(c1, Quaternion.identity);
 
-                sp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                sp.transform.SetPositionAndRotation(c2, Quaternion.identity);
+                
 
-                for (int i = 0; i <= stepSize; i++)
-                {
-                    var t = i / (float)stepSize;
-                    var point = Mathf.Pow((1 - t), 3) * p1 + 3 * Mathf.Pow((1 - t), 2) * t * c1 + 3 * (1 - t) * Mathf.Pow(t, 2) * c2 + Mathf.Pow(t, 3) * p2;
-                    lineRenderer.SetPosition(i, point);
-                }
+                //for (int i = 0; i <= stepSize; i++)
+                //{
+                //    var t = i / (float)stepSize;
+                //    var point = Mathf.Pow((1 - t), 3) * p1 + 3 * Mathf.Pow((1 - t), 2) * t * c1 + 3 * (1 - t) * Mathf.Pow(t, 2) * c2 + Mathf.Pow(t, 3) * p2;
+                //    lineRenderer.SetPosition(i, point);
+                //}
             }
         }
     }
@@ -105,10 +87,6 @@ class BezierCreator : MonoBehaviour
             for (int i = 0; i < controlPoints.Count; i++)
             {
                 Gizmos.DrawSphere(controlPoints[i], 0.5f);
-                //if (inputPoints.Count >= 2)
-                //{
-                //    Gizmos.DrawLine(inputPoints[i], inputPoints[i + 1]);
-                //}
             }
         }
         catch (System.Exception)
